2b96057c8ecf6fe814cf3a0887e3f608ad834c1b
unknown <naormordechaix@gmail.com>
2019-09-24 23:03:16 +0300

    added function for leave and start on touch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+
+let time = null;
+const app = document.querySelector('#app');
+const players = [];
+
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -9,7 +14,29 @@ function getRandomColor() {
     return color;
 }
 
-const touchActive = () => {
-    console.log('XXX')
-    document.querySelector('.box').style.background = getRandomColor();
+const touchActive = e => {
+    const id = Math.random().toString()
+    const node = document.createElement('div');
+    node.addEventListener('ontouchend', () => touchLeave(node))
+    node.setAttribute('id', id);
+    node.style.background = 'red';
+    node.style.position = 'absolute';
+    node.style.top = `${e.pageY}px`;
+    node.style.left = `${e.pageX}px`;
+    node.style.borderRadius = '50%';
+    node.style.width = '50px';
+    node.style.height = '50px';
+    document.getElementById('app').appendChild(node);
+    players.push(id);
 }
+
+const touchLeave = node => {
+    const children = app.childNodes;
+    let index = null;
+    for (let i = 0; i < children.length; i++) {
+        const currChild = children[i];
+        index = currChild.id === node.id ? i : null;
+    }
+    app.removeChild(app.childNodes(index));
+}
+
